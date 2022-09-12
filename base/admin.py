7428671a3d91ed87f4f2232b7ee49314cdc81e18
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2022-09-12 02:45:16 +0200

    finished admin panel and most of our schema

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Room, Topic, Message
+
+#! we are basically saying to the admin panel that we want to be able to view this item and also work with it in the built in admin panel
+
+admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
