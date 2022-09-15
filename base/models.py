c1c6c30803b5e504ed782db02008194cb75448fc
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2022-09-16 00:30:10 +0200

    now users can message

--- base/models.py
+++ base/models.py
@@ -15,6 +15,7 @@ class Room(models.Model):
     description = models.TextField(null=True, blank=True)
     topic = models.ForeignKey(Topic,on_delete=models.SET_NULL,null=True)
     host = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    participants = models.ManyToManyField(User,related_name='participants',blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
@@ -27,9 +28,9 @@ class Room(models.Model):
 class Message(models.Model):
     room = models.ForeignKey(Room,on_delete=models.CASCADE)
     body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.body[:50]
